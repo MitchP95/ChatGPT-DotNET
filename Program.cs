31358bb3cf0ad24986fe9f9e25a5eeb840c7ec8a
Mitchell Puersten <echo@Mitchells-MacBook-Pro.local>
2023-05-07 19:15:45 -0400

    Add console program with secret manager sample.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using ChatGPTTools;
+using Amazon;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+
+namespace ChatGPT_API_Example
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            string secretName = "";
+            string region = "";
+
+            IAmazonSecretsManager client = new AmazonSecretsManagerClient(RegionEndpoint.GetBySystemName(region));
+
+            GetSecretValueRequest request = new GetSecretValueRequest
+            {
+                SecretId = secretName,
+                VersionStage = "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified.
+            };
+
+            GetSecretValueResponse response;
+
+            try
+            {
+                response = await client.GetSecretValueAsync(request);
+            }
+            catch (Exception e)
+            {
+                // For a list of the exceptions thrown, see
+                // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+                throw e;
+            }
+
+            string secret = response.SecretString;
+
+            // TODO - add proper secret fetch, inject
+
+            var chatGPTClient = new ChatGPTClient(organizationKey, apiKey);
+
+            string prompt = "What is the capital of France?";
+            string response = await chatGPTClient.Query(prompt);
+
+            Console.WriteLine("Response: " + response);
+        }
+    }
+}
