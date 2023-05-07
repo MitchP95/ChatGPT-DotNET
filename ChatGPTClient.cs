04370ed855b5ff6e1e3b0c6c8c776f6fb7961094
Mitchell Puersten <mitch.puersten@gmail.com>
2023-05-07 16:30:21 -0400

    Fixed issues in class and default to gpt-3.5-turbo.

--- ChatGPTClient.cs
+++ ChatGPTClient.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 
@@ -6,53 +7,58 @@ namespace ChatGPTTools
 {
     public class ChatGPTClient
     {
+        private readonly string _organizationKey;
         private readonly string _apiKey;
         private readonly HttpClient _httpClient;
 
-        public ChatGPTClient(string apiKey)
+        public ChatGPTClient(string organizationKey, string apiKey)
         {
             _httpClient = new HttpClient();
+            _organizationKey = organizationKey;
             _apiKey = apiKey;
-
-            TestAuthorizationAsync().Wait();
         }
 
-        private async Task TestAuthorizationAsync()
-        {
-            var requestMessage = new HttpRequestMessage(HttpMethod.Get, "https://api.openai.com/v1/engines/davinci-codex")
-            {
-                Headers = { Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _apiKey) }
-            };
-
-            var response = await _httpClient.SendAsync(requestMessage);
-
-            if (!response.IsSuccessStatusCode)
-            {
-                throw new InvalidOperationException("Invalid API key or not authorized.");
-            }
-        }
-
-        public async Task<string> Query(string prompt)
+        public async Task<string> Query(string content, int maxTokens = 50)
         {
             var requestContent = new JObject
             {
-                {"prompt", prompt}
+                {"model", "gpt-3.5-turbo"},
+                {
+                    "messages", new JArray
+                    {
+                        new JObject
+                        {
+                            {"role", "user"},
+                            {"content", content}
+                        }
+                    }
+                },
+                {"max_tokens", maxTokens}
             };
 
-            var requestMessage = new HttpRequestMessage(HttpMethod.Post, "https://api.openai.com/v1/engines/davinci-codex/completions")
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post, "https://api.openai.com/v1/chat/completions")
             {
                 Content = new StringContent(requestContent.ToString(), System.Text.Encoding.UTF8, "application/json")
             };
 
             requestMessage.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _apiKey);
+            requestMessage.Headers.Add("OpenAI-Organization", _organizationKey);
 
-            var response = await _httpClient.SendAsync(requestMessage);
-            response.EnsureSuccessStatusCode();
+            try
+            {
+                var response = await _httpClient.SendAsync(requestMessage);
+                response.EnsureSuccessStatusCode();
 
-            var responseContent = await response.Content.ReadAsStringAsync();
-            var responseObject = JObject.Parse(responseContent);
+                var responseContent = await response.Content.ReadAsStringAsync();
+                var responseObject = JObject.Parse(responseContent);
 
-            return responseObject["choices"]?.First?["text"]?.ToString().Trim() ?? string.Empty;
+                return responseObject["choices"]?.First?["message"]?["content"]?.ToString().Trim() ?? string.Empty;
+            }
+            catch (HttpRequestException ex) when (ex.StatusCode == HttpStatusCode.TooManyRequests)
+            {
+                // You can return an appropriate message or implement a retry mechanism with a delay
+                return "Rate limit exceeded. Please wait before sending more requests.";
+            }
         }
     }
 }
